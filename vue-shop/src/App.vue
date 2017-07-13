76f3082ffb72abbd9ddec0142925ec7fb2c6cf51
xiaochenggit <707996290@qq.com>
2017-07-13 18:31:01 +0800

    json server
    模拟数据请求

---
+++ vue-shop/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <v-layout></v-layout>
+  </div>
+</template>
+
+<script>
+import Layout from '@/components/layout';
+export default {
+  name: 'app',
+  components: {
+    'v-layout': Layout
+  }
+}
+</script>
